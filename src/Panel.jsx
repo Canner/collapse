60c1533a089ab81b1dd782299669393f93840df9
abz53378 <a53378@yahoo.com.tw>
2018-02-01 02:02:43 +0800

    prop types

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { DraggableCore } from 'react-draggable';
 import PanelContent from './PanelContent';
